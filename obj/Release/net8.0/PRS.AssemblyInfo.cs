3f50ea9594ee129332c5a05ca5b8811cefd949fc
srijan-vaddadi <srijan.vaddadi@gmail.com>
2024-12-12 14:13:43 +0000

    final commit

--- obj/Release/net8.0/PRS.AssemblyInfo.cs
+++ obj/Release/net8.0/PRS.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRS")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85158a61071fe7fb201328b1fe77e0999688560b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRS")]
